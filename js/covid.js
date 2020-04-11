1741ce0850a2ab0e2ddb27dbbaeb5ca729998c07
Stephen D. Gibson <Stephen.d.gibson@gmail.com>
2020-04-11 11:27:19 -0400

    always create a new chart when switching countries

--- js/covid.js
+++ js/covid.js
@@ -2,7 +2,7 @@ function drawChart(country) {
 
     // make a get request for the canada spec and draw it when ready
     $.getJSON('https://raw.githubusercontent.com/dungeontiger/covid/master/specs/' + country + '_cases.json', function(spec) {
-        Plotly.plot('chartContainer', spec.data, spec.layout);
+        Plotly.newPlot('chartContainer', spec.data, spec.layout);
     });
 
       function country_handler(country) {
